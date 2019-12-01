fc7c5acbe415b2169325f7f1685ee2c075a9756b
abhishekgoyal1 <abhi.goyal2@gmail.com>
2019-12-01 11:40:12 +0530

    Updated my_small_regime to better reflect DARTS structure

--- network/RandWire.py
+++ network/RandWire.py
@@ -55,7 +55,8 @@ def conv_block(input, kernels, filters, strides, dropout_rate, training, scope, 
       input1= tf.layers.batch_normalization(input1, training= training)
       if (input2!= None):
         input= input2
-      input= tf.nn.relu(input)
+        input= tf.nn.relu(input)
+      
       input2 = tf.layers.conv2d(input, filters=filters, kernel_size=[1, 1], strides=[strides, strides], padding='SAME')
       input2= tf.layers.batch_normalization(input2, training= training)
 
@@ -157,6 +158,37 @@ def build_stage(input, filters, dropout_rate, training, graph_data, scope):
 
         return output
 
+def build_stage2(input, filters, dropout_rate, training, graph_data, scope):
+    graph, graph_order, start_node, end_node = graph_data
+
+    interms = {}
+    with tf.variable_scope(scope):
+        for node in graph_order:
+            if node in start_node:
+                interm = conv_block(input, 3, filters, 1, dropout_rate, training, scope='node' + str(node))
+                initInput= interm
+                interms[node] = interm
+            else:
+                in_node = list(nx.ancestors(graph, node))
+                if len(in_node) > 1:
+                    with tf.variable_scope('node' + str(node)):
+                        weight = tf.get_variable('sum_weight', shape=len(in_node), dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, 0, np.infty))
+                        weight = tf.nn.sigmoid(weight)
+                        interm = weight[0] * interms[in_node[0]]
+                        for idx in range(1, len(in_node)):
+                            interm += weight[idx] * interms[in_node[idx]]
+                        interm = conv_block(interm, 3, filters, 1, dropout_rate, training, scope='conv_block' + str(node), input2= initInput)
+                        interms[node] = interm
+                elif len(in_node) == 1:
+                    interm = conv_block(interms[in_node[0]], 3, filters, 1, dropout_rate, training, scope='node' + str(node), input2= initInput)
+                    interms[node] = interm
+
+        output = interms[end_node[0]]
+        for idx in range(1, len(end_node)):
+            output += interms[end_node[idx]]
+
+        return output
+
 def small_regime(input, stages, filters, classes, dropout_rate, graph_model, graph_param, graph_file_path, init_subsample, training):
     with tf.variable_scope('conv1'):
         if init_subsample is True:
@@ -225,7 +257,11 @@ def my_regime(input, stages, filters, classes, dropout_rate, graph_model, graph_
     return input
 
 def my_small_regime(input, stages, filters, classes, dropout_rate, graph_model, graph_param, graph_file_path, init_subsample, training): #regular regime 기반
-    for stage in range(1, stages+1):
+    stage=1
+    graph_data = gg.graph_generator(graph_model, graph_param, graph_file_path, 'conv' + str(stage) + '_' + graph_model)
+    input = build_stage2(input, filters, dropout_rate, training, graph_data, 'conv' + str(stage))
+    filters *= 2
+    for stage in range(2, stages+1):
         graph_data = gg.graph_generator(graph_model, graph_param, graph_file_path, 'conv' + str(stage) + '_' + graph_model)
         input = build_stage(input, filters, dropout_rate, training, graph_data, 'conv' + str(stage))
         filters *= 2
